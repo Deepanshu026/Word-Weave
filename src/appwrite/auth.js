2a87dadd21cc7e57e4031fc687c468685ded7d2f
Deepanshu026 <dp26dho@gmail.com>
2024-12-05 22:25:10 +0530

    auth service updated

--- src/appwrite/auth.js
+++ src/appwrite/auth.js
@@ -21,7 +21,7 @@ export class AuthService {
         name
       );
       if (userAccount) {
-        
+        // call another method
         return this.login({ email, password });
       } else {
         return userAccount;
